c82725ffc196acf064845af7d9ab34b1247e8d58
yogi <yogi@gmail.cOm>
2023-02-11 02:11:53 +0530

    add cred

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "terraform"
+        storage_account_name = "tfstorefile"
+        container_name       = "tfstbackend01"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
