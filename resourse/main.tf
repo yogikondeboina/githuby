f444766f9ed2d0eacaaad782148813f638bd3cd9
yogi <yogi@gmail.cOm>
2023-02-11 14:42:51 +0530

    removve

--- resourse/main.tf
+++ resourse/main.tf
@@ -9,7 +9,7 @@ terraform {
         resource_group_name  = "terraform"
         storage_account_name = "tfstorefile"
         container_name       = "tfstbackend01"
-        key                  = "terraform.tfstate"
+        key                  = "rgterraform.tfstate"
     }
 
 }
@@ -23,11 +23,3 @@ resource "azurerm_resource_group" "state-demo-secure" {
   location = "eastus"
 }
 
-# module "storage_account" {
-#   source = ".azservices/storageaccount"
-#   location = var.location
-#   resource_group = var.azurerm_resource_group
-#   storage_name = var.azurerm_storage_account
-#   resource_group_name = var.resource_group_name
-#   azurem_storage_account = var.azurem_storage_account 
-# }
